a45ffe9a0a33bfbe64536e3c12cee78f9e788439
Rod Johnson <johnsonroda@gmail.com>
2018-03-12 20:04:42 +1100

    Initial commit from Atomist

---
+++ src/test/java/atomist/X1ApplicationTests.java
@@ -0,0 +1,16 @@
+package atomist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class X1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
