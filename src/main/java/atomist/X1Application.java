ea2a7a78bc73b82498703a1225df908cf22537ea
Rod Johnson <johnsonroda@gmail.com>
2018-03-12 21:08:45 +1100

    Everyone needs encouragement to write Java

--- src/main/java/atomist/X1Application.java
+++ src/main/java/atomist/X1Application.java
@@ -1,5 +1,7 @@
 // You are the delta in what you do, not the things you did in the past
 
+// You are the delta in what you do, not the things you did in the past
+
 // As I say yes to life, life says yes to me.
 
 // Deep at the center of my being is an infinite well of love.
